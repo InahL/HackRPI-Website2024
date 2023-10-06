9aee13a9aec2623beda6469ef7ee5139d55e5330
InahL <inah0331@gmail.com>
2023-10-06 18:25:10 -0400

    create new page

---
+++ frontend/src/Pages/SponsorPage/SponsorPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const SponsorPage = () => {
+  return (
+    <div id="sponsor" 
+    className="SponsorPage" 
+    style = {{
+      testAlign: 'center', 
+      width: "80%", 
+      margin: 'auto'
+      }}
+      >
+        <h1 
+        className="header"style={{ 
+          fontFamily: 'Mokoto', 
+          fontSize: 46, 
+          height: 35 
+          }}
+          >
+            Sponsorship
+        </h1>
+    </div>
+  );
+}
+
+export default SponsorPage;
